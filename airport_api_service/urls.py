54b9e1a77ceedbaf075fc1fe310a24d020832a2d
vburkalo <vitaliyburkalo81@gmail.com>
2024-05-25 15:02:13 +0300

    updated project endpoints to include api & flights & users & swagger & redoc urls & static files

--- airport_api_service/urls.py
+++ airport_api_service/urls.py
@@ -1,7 +1,23 @@
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
+from drf_spectacular.views import SpectacularSwaggerView, SpectacularRedocView
+
+from django.conf import settings
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("api/", include("flights.urls")),
-]
+    path("api/flights/", include("flights.urls")),
+    path("api/users/", include("users.urls")),
+    path(
+        "api/doc/swagger/",
+        SpectacularSwaggerView.as_view(url_name="schema"),
+        name="swagger-ui",
+    ),
+    path(
+        "api/doc/redoc/",
+        SpectacularRedocView.as_view(url_name="schema"),
+        name="redoc",
+    ),
+    path("__debug__/", include("debug_toolbar.urls")),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
