43dfa3b9a1b42ae5d6a5c111baab005ae04831f7
vburkalo <vitaliyburkalo81@gmail.com>
2024-05-25 15:34:54 +0300

    added new permission class to viewsets

--- flights/views.py
+++ flights/views.py
@@ -10,6 +10,7 @@ from flights.models import (
     Order,
     Ticket,
 )
+from flights.permissions import IsAdminOrIfAuthenticatedReadOnly
 from flights.serializers import (
     CountrySerializer,
     CitySerializer,
@@ -26,43 +27,52 @@ from flights.serializers import (
 class CountryViewSet(viewsets.ModelViewSet):
     queryset = Country.objects.all().select_related("city")
     serializer_class = CountrySerializer
+    permission_classes = (IsAdminOrIfAuthenticatedReadOnly,)
 
 
 class CityViewSet(viewsets.ModelViewSet):
     queryset = City.objects.all().select_related("country")
     serializer_class = CitySerializer
+    permission_classes = (IsAdminOrIfAuthenticatedReadOnly,)
 
 
 class AirportViewSet(viewsets.ModelViewSet):
     queryset = Airport.objects.all().select_related("closest_big_city__country")
     serializer_class = AirportSerializer
+    permission_classes = (IsAdminOrIfAuthenticatedReadOnly,)
 
 
 class AirplaneTypeViewSet(viewsets.ModelViewSet):
     queryset = AirplaneType.objects.all()
     serializer_class = AirplaneTypeSerializer
+    permission_classes = (IsAdminOrIfAuthenticatedReadOnly,)
 
 
 class AirplaneViewSet(viewsets.ModelViewSet):
     queryset = Airplane.objects.all().select_related("airplane_type")
     serializer_class = AirplaneSerializer
+    permission_classes = (IsAdminOrIfAuthenticatedReadOnly,)
 
 
 class RouteViewSet(viewsets.ModelViewSet):
     queryset = Route.objects.all().select_related("source", "destination")
     serializer_class = RouteSerializer
+    permission_classes = (IsAdminOrIfAuthenticatedReadOnly,)
 
 
 class FlightViewSet(viewsets.ModelViewSet):
     queryset = Flight.objects.all().select_related("route", "airplane")
     serializer_class = FlightSerializer
+    permission_classes = (IsAdminOrIfAuthenticatedReadOnly,)
 
 
 class OrderViewSet(viewsets.ModelViewSet):
     queryset = Order.objects.all().select_related("user")
     serializer_class = OrderSerializer
+    permission_classes = (IsAdminOrIfAuthenticatedReadOnly,)
 
 
 class TicketViewSet(viewsets.ModelViewSet):
     queryset = Ticket.objects.all().select_related("flight", "order")
     serializer_class = TicketSerializer
+    permission_classes = (IsAdminOrIfAuthenticatedReadOnly,)
