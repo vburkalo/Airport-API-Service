6ab037d5c75036209b70c85a0e2dbd5ede40832e
vburkalo <vitaliyburkalo81@gmail.com>
2024-05-25 14:31:13 +0300

    created models for flights app

--- flights/models.py
+++ flights/models.py
@@ -1,3 +1,83 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Country(models.Model):
+    name = models.CharField(max_length=64)
+
+    def __str__(self):
+        return self.name
+
+
+class City(models.Model):
+    name = models.CharField(max_length=64)
+    country = models.ForeignKey(Country, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
+
+
+class Airport(models.Model):
+    name = models.CharField(max_length=64)
+    closest_big_city = models.ForeignKey(City, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
+
+
+class AirplaneType(models.Model):
+    name = models.CharField(max_length=64)
+
+    def __str__(self):
+        return self.name
+
+
+class Airplane(models.Model):
+    name = models.CharField(max_length=64)
+    rows = models.IntegerField()
+    seats_in_row = models.IntegerField()
+    airplane_type = models.ForeignKey(AirplaneType, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
+
+
+class Route(models.Model):
+    source = models.ForeignKey(
+        Airport, related_name="routes_from", on_delete=models.CASCADE
+    )
+    destination = models.ForeignKey(
+        Airport, related_name="routes_to", on_delete=models.CASCADE
+    )
+    distance = models.IntegerField()
+
+    def __str__(self):
+        return f"{self.source} to {self.destination}"
+
+
+class Flight(models.Model):
+    route = models.ForeignKey(Route, on_delete=models.CASCADE)
+    airplane = models.ForeignKey(Airplane, on_delete=models.CASCADE)
+    departure_time = models.DateTimeField()
+    arrival_time = models.DateTimeField()
+
+    def __str__(self):
+        return f"Flight {self.id} on route {self.route}"
+
+
+class Order(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"Order {self.id} by {self.user}"
+
+
+class Ticket(models.Model):
+    row = models.IntegerField()
+    seat = models.IntegerField()
+    flight = models.ForeignKey(Flight, on_delete=models.CASCADE)
+    order = models.ForeignKey(Order, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"Ticket {self.id} for flight {self.flight}"
