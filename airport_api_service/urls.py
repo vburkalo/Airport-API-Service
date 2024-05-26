98feeabde4261f51b66ed3889fc4558562383bb1
vburkalo <vitaliyburkalo81@gmail.com>
2024-05-26 14:54:32 +0300

    fixed api/schema endpoint to have correct view & set namespaces for the flights and users endpoints

--- airport_api_service/urls.py
+++ airport_api_service/urls.py
@@ -1,15 +1,15 @@
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
-from drf_spectacular.views import SpectacularSwaggerView, SpectacularRedocView
+from drf_spectacular.views import SpectacularSwaggerView, SpectacularRedocView, SpectacularAPIView
 
 from django.conf import settings
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("api/flights/", include("flights.urls")),
-    path("api/users/", include("users.urls")),
-    path("api/schema/", SpectacularSwaggerView.as_view(), name="schema"),
+    path("api/flights/", include("flights.urls", namespace="flights")),
+    path("api/users/", include("users.urls", namespace="users")),
+    path("api/schema/", SpectacularAPIView.as_view(), name="schema"),
     path(
         "api/doc/swagger/",
         SpectacularSwaggerView.as_view(url_name="schema"),
