599ab66912970b6431e4ea8e6cc5f1006a743e31
vburkalo <vitaliyburkalo81@gmail.com>
2024-05-26 19:59:56 +0300

    updated settings.py of the project with hidden SECRET KEY, SECURED DEBUG, SECURED ALLOWED_HOSTS & new Postgresql Database

--- airport_api_service/settings.py
+++ airport_api_service/settings.py
@@ -1,7 +1,10 @@
+import os
 from datetime import timedelta
 from pathlib import Path
-
+from dotenv import load_dotenv
 import flights.permissions
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -10,12 +13,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-ry923@gzcn)7!)#x^sk_h1z#13uh%)6f*1!4&u(02$*=x#g=4w"
+SECRET_KEY = os.getenv("SECRET_KEY", )
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv("DEBUG", "False") == "True"
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", "").split()
 
 INTERNAL_IPS = [
     "127.0.0.1",
@@ -75,8 +78,12 @@ WSGI_APPLICATION = "airport_api_service.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": os.getenv("POSTGRES_DB"),
+        "USER": os.getenv("POSTGRES_USER"),
+        "PASSWORD": os.getenv("POSTGRES_PASSWORD"),
+        "HOST": os.getenv("POSTGRES_HOST"),
+        "PORT": os.getenv("POSTGRES_PORT"),
     }
 }
 
@@ -86,7 +93,7 @@ DATABASES = {
 AUTH_PASSWORD_VALIDATORS = [
     {
         "NAME": "django.contrib.auth.password_validation."
-        "UserAttributeSimilarityValidator",
+                "UserAttributeSimilarityValidator",
     },
     {
         "NAME": "django.contrib.auth.password_validation." "MinimumLengthValidator",
@@ -139,7 +146,6 @@ REST_FRAMEWORK = {
         "flights.permissions.IsAdminOrIfAuthenticatedReadOnly",
     ],
 }
-
 
 SPECTACULAR_SETTINGS = {
     "TITLE": "Airport API Service",
