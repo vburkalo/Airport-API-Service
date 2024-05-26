47472c0b90e22b56539a66424145ef42180e4873
vburkalo <vitaliyburkalo81@gmail.com>
2024-05-26 19:32:32 +0300

    added some more changes to serializers.py of the flights

--- flights/serializers.py
+++ flights/serializers.py
@@ -68,7 +68,6 @@ class FlightSerializer(serializers.ModelSerializer):
 
 
 class OrderSerializer(serializers.ModelSerializer):
-
     class Meta:
         model = Order
         fields = ("id", "created_at", "user")
