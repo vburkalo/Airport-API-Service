d8b6b29dbc5d848af91e7034daf4a7404f7246e0
vburkalo <vitaliyburkalo81@gmail.com>
2024-05-28 00:25:31 +0300

    made migrations

---
+++ flights/migrations/0007_auto_20240528_0021.py
@@ -0,0 +1,19 @@
+from django.db import migrations, models
+
+
+def populate_code(apps, schema_editor):
+    Airport = apps.get_model('flights', 'Airport')
+    for airport in Airport.objects.all():
+        airport.code = "DCA"
+        airport.save()
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('flights', '0006_airport_code'),
+    ]
+
+    operations = [
+        migrations.RunPython(populate_code),
+    ]
