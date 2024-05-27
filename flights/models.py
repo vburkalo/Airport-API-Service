a8966fd44d9a08213565a35d98392a6591f7fda6
vburkalo <vitaliyburkalo81@gmail.com>
2024-05-27 16:31:14 +0300

    added ImageField to Aiplane model in airport_api_service models to post images & added function to return calculated capacity of the plane

--- flights/models.py
+++ flights/models.py
@@ -1,6 +1,10 @@
+import os
+import uuid
+
 from django.conf import settings
 from django.db import models
 from django.contrib.auth.models import User
+from django.utils.text import slugify
 
 
 class Country(models.Model):
@@ -33,11 +37,26 @@ class AirplaneType(models.Model):
         return self.name
 
 
+def airplane_image_file_path(instance, filename):
+    _, extension = os.path.splitext(filename)
+    filename = f"{slugify(instance.name)}-{uuid.uuid4()}{extension}"
+
+    return os.path.join("uploads/airplanes/", filename)
+
+
 class Airplane(models.Model):
     name = models.CharField(max_length=64)
     rows = models.IntegerField()
     seats_in_row = models.IntegerField()
     airplane_type = models.ForeignKey(AirplaneType, on_delete=models.CASCADE)
+    image = models.ImageField(null=True, upload_to=airplane_image_file_path)
+
+    class Meta:
+        ordering = ["name"]
+
+    @property
+    def capacity(self) -> int:
+        return self.rows * self.seats_in_row
 
     def __str__(self):
         return self.name
